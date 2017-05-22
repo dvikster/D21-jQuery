3262e23a046f44a33596f50b0e60f1be9a3bf3be
dvikster <dvikster@gmail.com>
2017-05-22 16:39:20 +0300

    added

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+// jQuery=$;
 $(document).ready(function () {
     $('button').on('click', multiplyInput);
 });
@@ -6,9 +7,12 @@ function multiplyInput(){
     // var one = $('#one').val();
     // var two = $('#two').val();
     // var multiplyAnswer = one*two;
-    var multiplyAnswer = $('#one').val()*$('#two').val();
 
-    if(multiplyAnswer >=100) {
+    var multiplyAnswer = $('#one').val()*$('#two').val();
+    console.log(multiplyAnswer);
+    $('#out').html(multiplyAnswer);
+
+    if(multiplyAnswer >=100) {s
         $('#out').css('color','red');
     }
     else {
